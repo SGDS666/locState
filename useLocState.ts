82954ef182ff45192ace62e3a8be0f7b6faad4da
张森 <86196091+SGDS666@users.noreply.github.com>
2023-02-09 04:11:59 +0800

    Update useLocState.ts

--- useLocState.ts
+++ useLocState.ts
@@ -1,24 +1,26 @@
 import { useEffect, useLayoutEffect, useState } from "react";
-interface TM<T>{
+interface TM<T> {
     [key: string]: T;
 }
-const M:TM<any> = {}
+const M: TM<any> = {}
+
+
 const getLocData = (key: string) => {
     const valueString = localStorage.getItem("locState")
     const valueobj: { [key: string]: any } = JSON.parse(valueString ?? "{}")
     const selfState = valueobj[key]
-    if(selfState!==null&&selfState!==undefined){
+    if (selfState !== null && selfState !== undefined) {
         M[key] = selfState
         return selfState
     }
-    
-    
+
+
 }
-const saveLocData = <T>(key: string,newValue:T) => {
+const saveLocData = <T>(key: string, newValue: T) => {
     const DBState: { [key: string]: any } = JSON.parse(localStorage.getItem("locState") ?? "{}")
     M[key] = newValue
     DBState[key] = newValue
-    
+
     localStorage.setItem("locState", JSON.stringify(DBState));
 
 
@@ -34,43 +36,52 @@ export const useLocState = <T>(key: string, value: T): [T, (newValue: T) => void
         if (locState) {
             setState(locState)
         }
-        
+
 
 
     }, [])
-    useLayoutEffect(()=>{//loc无值时将state存储进loc
-        if(!locState){
-            saveLocData(key,state)
+    useLayoutEffect(() => {//loc无值时将state存储进loc
+        if (!locState) {
+            saveLocData(key, state)
         }
-    },[])
+    }, [])
 
     const SetLocState = (newValue: T) => {
 
         setState(preValue => {
-            saveLocData(key,newValue)
+            saveLocData(key, newValue)
             return newValue
         })
     }
 
     return [state, SetLocState];
 }
-export const useLocValue = (key: string) =>{
-    const [locValue,setLocValue] = useState(null)
-    useEffect(()=>{
-        if(locValue===null){
-            setTimeout(() => {
-                setLocValue( getLocData(key))
-            }, 10);
-            
+export const useLocValue = (key: string) => {
+    const [locValue, setLocValue] = useState(null)
+    const newValue = getLocData(key)
+    useEffect(() => {
+        if(locValue===null||locValue===undefined){
+            setLocValue(newValue)
+            return
         }
-    },[locValue])
+        if (Array.isArray(locValue) && Array.isArray(newValue) ) {
+            //@ts-ignore
+            if (newValue.length !== locValue.length) {
+                //@ts-ignore
+                setLocValue(newValue)
+                return
+            }
+        }
+        
 
-    if(locValue){
+    }, [newValue])
+
+    if (locValue) {
         return locValue
     }
-    return M[key]//缓存数据 未读取loc前方便其他组件读取
+    return M[key]
 
-    
+
 
 }
 
